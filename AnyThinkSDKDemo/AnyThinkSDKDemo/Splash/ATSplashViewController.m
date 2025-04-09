69f0a159b429edc4142b411ad6dbd61f23b8f614
eclicktechwang <yixu.wang@eclicktech.com.cn>
2025-04-09 13:40:22 +0800

    update topon

--- AnyThinkSDKDemo/AnyThinkSDKDemo/Splash/ATSplashViewController.m
+++ AnyThinkSDKDemo/AnyThinkSDKDemo/Splash/ATSplashViewController.m
@@ -202,11 +202,12 @@
     // 设置开屏广告中支持广告源设置加载超时时间，并不是整个广告位请求的时间
 //    [mutableDict setValue:@5.5 forKey:kATSplashExtraTolerateTimeoutKey];
     
-    [[ATAdManager sharedManager] loadADWithPlacementID:self.placementID
-                                                 extra:mutableDict
-                                              delegate:self
-                                         containerView:label
-                                 defaultAdSourceConfig:self.modelButton.isSelected ? self.defaultAdSourceConfigStr : nil];
+    [[ATAdManager sharedManager] loadADWithPlacementID:self.placementID extra:mutableDict delegate:self containerView:label];
+//    [[ATAdManager sharedManager] loadADWithPlacementID:self.placementID
+//                                                 extra:mutableDict
+//                                              delegate:self
+//                                         containerView:label
+//                                 defaultAdSourceConfig:self.modelButton.isSelected ? self.defaultAdSourceConfigStr : nil];
 }
 
 - (UIInterfaceOrientation)currentInterfaceOrientation {
